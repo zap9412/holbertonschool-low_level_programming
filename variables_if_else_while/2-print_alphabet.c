7d247f61cbd2225928b03f4a0f3bc7d317db9afa
zap9412 <thelyaan444@hotmail.com>
2025-10-21 12:44:03 +0000

    Betty Holberton C Program

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+	putchar('\n');
+
+	return (0);
+}
