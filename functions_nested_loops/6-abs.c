d7323b07021b3ad7f776c1b8d5ec06419cbeb049
votre_nom <votre_email>
2025-10-23 09:48:21 +0000

    Add holbertonschool-shell dir Project

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - compoutes the absolute value of an integer
+ * @n: the integer to check
+ *
+ * Return: the absolute of n
+ */
+ int _abs(int n)
+ {
+    if (n < 0)
+    {
+        return (-n);
+    }
+    return (n);
+ }
+ 
