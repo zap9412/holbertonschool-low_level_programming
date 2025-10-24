7d314cce20b102e384fbf7019df920e2d4494f14
votre_nom <votre_email>
2025-10-24 17:45:13 +0000

    Add holbertonschool-shell dir Project

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table, starting with 0
+ */
+void times_table(void)
+{
+	int i, j, prod;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			prod = i * j;
+			if (j != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				if (prod < 10)
+					_putchar(' ');
+			}
+			if (prod >= 10)
+				_putchar((prod / 10) + '0');
+			_putchar((prod % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
