d0dc2f76ce5c9bc91bcb30a09b2f6eff11e3468c
votre_nom <votre_email>
2025-10-24 17:38:23 +0000

    Add holbertonschool-shell dir Project

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,7 +1,33 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+
+/* _putchar.c */
 int _putchar(char c);
+
+/* 1-alphabet.c */
+void print_alphabet(void);
+
+/* 2-print_alphabet_x10.c */
+void print_alphabet_x10(void);
+
+/* 3-islower.c */
+int _islower(int c);
+
+/* 4-isalpha.c */
+int _isalpha(int c);
+
+/* 5-sign.c */
+int print_sign(int n);
+
+/* 6-abs.c */
+int _abs(int n);
+
+/* 7-print_last_digit.c */
 int print_last_digit(int n);
+
+/* 8-24_hours.c */
+void jack_bauer(void);
 
 #endif /* MAIN_H */
