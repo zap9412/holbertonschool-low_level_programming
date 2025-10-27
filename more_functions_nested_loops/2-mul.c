697e482e281ff861cfe9a41f0dac913cf4104096
zap9412 <thelyaan444@hotmail.com>
2025-10-27 13:19:20 +0000

    Add holbertonschool-shell dir Project

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiplie deux entiers
+ * @a: premier entier
+ * @b: deuxième entier
+ *
+ * Return: le résultat de la multiplication de a et b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
