06ee2e70439c9bb111215f72b3b8963290ffbb00
zap9412 <thelyaan444@hotmail.com>
2025-10-27 13:14:52 +0000

    Add holbertonschool-shell dir Project

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isdigit - vérifie si un caractère est un chiffre (0 à 9)
+ * @c: caractère à vérifier
+ *
+ * Return: 1 si c est un chiffre, 0 sinon
+ */
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	else
+		return (0);
+}
