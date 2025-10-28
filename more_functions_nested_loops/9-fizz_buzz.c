a4fc304c97a909eb3a3cd7afe051da66b20c7166
votre_nom <votre_email>
2025-10-28 10:15:47 +0000

    Add holbertonschool-shell dir Project

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - imprime les nombres de 1 à 100 avec Fizz/Buzz
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz");
+		else if (i % 3 == 0)
+			printf("Fizz");
+		else if (i % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d", i);
+
+		if (i != 100)
+			printf(" ");
+	}
+
+	printf("\n");
+
+	return (0);
+}
