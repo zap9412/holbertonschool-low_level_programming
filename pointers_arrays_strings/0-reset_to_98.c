5b11f831b3beea5130374c2f9fcf475097017762
votre_nom <votre_email>
2025-10-28 10:37:21 +0000

    Add holbertonschool-shell dir Project

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - met la valeur pointée par n à 98
+ * @n: pointeur vers l'entier à modifier
+ *
+ * Return: rien
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
