1a94a0adf8a3f9537c6874f7c52f4491b8410d3f
zap9412 <thelyaan444@hotmail.com>
2025-11-05 13:54:14 +0000

    Add holbertonschool-shell dir Project

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -2,22 +2,21 @@
 #include <stddef.h>
 
 /**
- *
  * _strchr - locates a character in a string
  * @s: string to search in
  * @c: character to find
  *
- * Return: pointer to first occurence of c in s, or NULL if not found
+ * Return: pointer to first occurrence of c in s, or NULL if not found
  */
 char *_strchr(char *s, char c)
 {
-    while (*s)
-    {
-        if (*s == c)
-            return (s);
-        s++;
-    }
-    if (*s == c) /* Vérifie aussi le '\0' */
-        return (s);
-    return (NULL);
+	while (*s)
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
+	if (*s == c) /* Vérifie aussi le '\0' */
+		return (s);
+	return (NULL);
 }
