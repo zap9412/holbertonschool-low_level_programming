32d314f04ee03cc4765986fc901bddfc9afb5666
zap9412 <thelyaan444@hotmail.com>
2025-11-10 14:44:13 +0000

    Add holbertonschool-shell dir Project

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * simple_print_buffer - prints buffer in hexa
+ * @buffer: the address of memory to print
+ * @size: the size of the memory to print
+ *
+ * Return: Nothing.
+ */
+void simple_print_buffer(char *buffer, unsigned int size)
+{
+    
+}
