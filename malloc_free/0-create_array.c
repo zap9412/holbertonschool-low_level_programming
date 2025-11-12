357a74a1b91c224553146a9121d910f528187326
zap9412 <thelyaan444@hotmail.com>
2025-11-12 11:04:34 +0000

    Add holbertonschool-shell dir Project

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -3,25 +3,26 @@
 
 /**
  * create_array - creates an array of chars and initializes it with a specific char
- * @size: the size of the array
- * @c: the char to fill the array with
+ * @size: size of the array to allocate
+ * @c: char to initialize each element with
  *
- * Return: pointer to the array, or NULL if it fails or size is 0
+ * Return: pointer to the allocated array,
+ * or NULL if size == 0 or allocation fails
  */
 char *create_array(unsigned int size, char c)
 {
-    char *array;
-    unsigned int i;
+	char *array;
+	unsigned int i;
 
-    if (size == 0)
-        return (NULL);
+	if (size == 0)
+		return (NULL);
 
-    array = malloc(sizeof(char) * size);
-    if (array == NULL)
-        return (NULL);
+	array = malloc(sizeof(char) * size);
+	if (array == NULL)
+		return (NULL);
 
-    for (i = 0; i < size; i++)
-        array[i] = c;
+	for (i = 0; i < size; i++)
+		array[i] = c;
 
-    return (array);
+	return (array);
 }
