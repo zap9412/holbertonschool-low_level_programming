3caf6cda145f1b87fb3396506e255f046a91ee0c
zap9412 <thelyaan444@hotmail.com>
2025-11-19 12:47:44 +0000

    Add holbertonschool-shell dir Project

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -18,12 +18,10 @@ int sum_them_all(const unsigned int n, ...)
         return (0);
 
     va_start(args, n);
-
     for (i = 0; i < n; i++)
     {
         sum += va_arg(args, int);
     }
-
     va_end(args);
 
     return (sum);
