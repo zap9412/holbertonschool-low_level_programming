dc12a4400ebd42a1cb2a2877f7ac8bca7f19d121
zap9412 <thelyaan444@hotmail.com>
2025-12-03 12:08:20 +0100

    Singly linked lists

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -4,34 +4,34 @@
 
 /**
  * add_node - adds a new node at the beginning of a list_t list
- * @head: pointer to the pointer to the beginning of the list
- * @str: string to duplicate and store in the new node
- * 
+ * @head: pointer to pointer to list
+ * @str: string to duplicate
+ *
  * Return: address of the new element, or NULL if it failed
  */
 list_t *add_node(list_t **head, const char *str)
 {
-    list_t *new;
-    char *dup;
+	list_t *new_node;
+	char *dup;
 
-    if (str == NULL)
-        return (NULL);
+	if (str == NULL)
+		return (NULL);
 
-    dup = strdup(str);
-    if (dup == NULL)
-        return (NULL);
+	dup = strdup(str);
+	if (dup == NULL)
+		return (NULL);
 
-    new = malloc(sizeof(list_t));
-    if (new == NULL)
-    {
-        free(dup);
-        return (NULL);
-    }
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+	{
+		free(dup);
+		return (NULL);
+	}
 
-    new->str = dup;
-    new->len = strlen(str);
-    new->next = *head;
-    *head = new;
+	new_node->str = dup;
+	new_node->len = strlen(str);
+	new_node->next = *head;
+	*head = new_node;
 
-    return (new);
+	return (new_node);
 }
